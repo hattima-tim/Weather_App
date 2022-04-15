7f7d81b42b6c2f6c98118847b68543596e1a8069
ridwan <irfanridwan20@gmail.com>
2022-04-15 22:38:03 +0600

    changed the function name getWeatherData to sendReqForWeatherData

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const getWeatherData= async function (location) {
+const sendReqForWeatherData= async function (location) {
     try {
         const url=`http://api.openweathermap.org/data/2.5/weather?q=${ location }&APPID=6a45a6a1f9ac0dde88276011ee3a19fe`;
         const response=await fetch(url, { mode: "cors" });
@@ -9,4 +9,4 @@ const getWeatherData= async function (location) {
     }
 
 };
-getWeatherData('dhaka');
+sendReqForWeatherData('dhaka');
