67ee265409544bc749a7ba01b0936a92c6397aa4
ridwan <irfanridwan20@gmail.com>
2022-04-18 10:24:52 +0600

    https over http
    changed the http in API call link to https because using http link is throwing mixed content error.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sendReqForWeatherData= async function (location) {
     try {
-        const url=`http://api.openweathermap.org/data/2.5/weather?q=${ location }&APPID=6a45a6a1f9ac0dde88276011ee3a19fe&units=metric`;
+        const url=`https://api.openweathermap.org/data/2.5/weather?q=${ location }&APPID=6a45a6a1f9ac0dde88276011ee3a19fe&units=metric`;
         const response=await fetch(url, { mode: "cors" });
         const weatherData=await response.json();
         return weatherData;
